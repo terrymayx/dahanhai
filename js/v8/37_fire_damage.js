cb922523851a84d893276a5b37fd67761ef989ae
terrymayx <45205504+terrymayx@users.noreply.github.com>
2026-09-03 03:50:27 +0800

    V9.4: increase deck durability and tune fire

--- js/v8/37_fire_damage.js
+++ js/v8/37_fire_damage.js
@@ -2,17 +2,17 @@
   'use strict';
 
   const G=root.V8ShipGrid,B=root.V8Battle;
-  if(!G||!B)throw new Error('V9.3 fire damage requires grid and battle');
+  if(!G||!B)throw new Error('V9.4 fire damage requires grid and battle');
 
-  const DECK_HP_MULT=3;
-  const FIRE_CHANCE={deck:.28,hull:.18};
-  const FIRE_DPS=5.5;
+  const DECK_HP_MULT=4;
+  const FIRE_CHANCE={deck:.24,hull:.16};
+  const FIRE_DPS=4.8;
   const originalCreateTemplateShip=G.createTemplateShip;
   const originalDamageCell=G.damageCell;
 
   function prepareShip(ship){
-    if(!ship||ship.__v93FirePrepared)return ship;
-    ship.__v93FirePrepared=true;
+    if(!ship||ship.__v94FirePrepared)return ship;
+    ship.__v94FirePrepared=true;
     for(const cell of ship.cells||[]){
       cell.burning=false;
       cell.fireAge=0;
@@ -59,9 +59,9 @@
     cell.fireDamage=0;
     const pos=G.cellCenterWorld(ship,cell);
     if(state&&state.fx){
-      state.fx.push({k:'structureBreak',x:pos.x,y:pos.y,t:0,dur:.42,r:26});
-      state.fx.push({k:'waterSplash',x:pos.x,y:pos.y,t:0,dur:.38,r:18});
-      state.fx.push({k:'waterRing',x:pos.x,y:pos.y,t:0,dur:.58,r:19});
+      state.fx.push({k:'structureBreak',x:pos.x,y:pos.y,t:0,dur:.42,r:22});
+      state.fx.push({k:'waterSplash',x:pos.x,y:pos.y,t:0,dur:.38,r:15});
+      state.fx.push({k:'waterRing',x:pos.x,y:pos.y,t:0,dur:.58,r:16});
     }
     if(state&&typeof state.onCellDestroyed==='function'){
       try{state.onCellDestroyed(ship,cell,pos,{side:'fire',damage:FIRE_DPS,fire:true});}catch(e){}
@@ -93,5 +93,5 @@
     updateFire(state,dt);
   };
 
-  root.V93FireDamage={prepareShip,maybeIgnite,updateFire,DECK_HP_MULT,FIRE_CHANCE,FIRE_DPS};
+  root.V94FireDamage={prepareShip,maybeIgnite,updateFire,DECK_HP_MULT,FIRE_CHANCE,FIRE_DPS};
 })(typeof globalThis!=='undefined'?globalThis:this);
