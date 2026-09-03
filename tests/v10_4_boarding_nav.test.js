af545a73886cb61ca9e6a508e8beea60310c7511
terrymayx <45205504+terrymayx@users.noreply.github.com>
2026-09-03 19:10:19 +0800

    test: require automatic V10.4 nav invalidation on deck destruction

--- tests/v10_4_boarding_nav.test.js
+++ tests/v10_4_boarding_nav.test.js
@@ -2,6 +2,7 @@
 const {assert,makeContext,makeState,loadBoarding}=require('./v10_4_test_helpers');
 const ctx=makeContext(),st=makeState(ctx),V=loadBoarding(ctx);
 const nav1=V.buildWalkable(st);assert.ok(nav1.nodes.length>0);
-const dead=st.player.cells.find(c=>c.type==='deck');assert.ok(dead);dead.alive=false;V.markStructureDirty(st.player);
+const dead=st.player.cells.find(c=>c.type==='deck');assert.ok(dead);
+const res=ctx.V8ShipGrid.damageCell(st.player,dead,dead.hp+1);assert.equal(res.destroyed,true);
 const nav2=V.buildWalkable(st);assert.equal(nav2.byKey[dead.gx+','+dead.gy],undefined);assert.notEqual(nav1.revision,nav2.revision);
 console.log('V10.4 boarding nav: PASS');
